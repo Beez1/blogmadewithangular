aad22135f733a3c6a81eb4558f88799c337faa3e
BeEz <98581184+Beez1@users.noreply.github.com>
2024-08-05 22:19:46 +0100

    Refactor home component to display questions dynamically

--- angularjsblog/src/app/home/home.component.ts
+++ angularjsblog/src/app/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+interface Model {
+  _id: string;
+  username: string;
+  title: string;
+  question: string;
+  category: string;
+  createdAt: string | number | Date;
+  updatedAt: string | number | Date;
+}
 
 @Component({
   selector: 'app-home',
@@ -11,6 +22,34 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
+    model: Model[] = [];
+    searchQuery: string = '';
+  
+    constructor(private http: HttpClient, private route: ActivatedRoute) {}
+  
+    ngOnInit() {
+      this.fetchModel(); // Fetch questions when component initializes
+    }
+  
+    fetchModel() {
+      let apiUrl = 'http://localhost:4201/model';
+  
+      if (this.searchQuery.trim() !== '') {
+        apiUrl += `?search=${this.searchQuery.trim()}`;
+      }
+  
+      this.http.get<{questions: Model[]}>(apiUrl)
+        .subscribe({
+          next: (response) => {
+            this.model = response.questions;
+            console.log('Questions fetched successfully:');
+          },
+          error: (error) => {
+            console.error('Error fetching questions:', error);
+          }
+        });
+    }
 
 }
